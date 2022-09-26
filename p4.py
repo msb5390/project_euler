5b66546f3587736571651314d174f1794e17a3d7
Michael Bell <msb5390@gmail.com>
2022-09-25 20:27:23 -0500

    solved problem 4

--- p4.py
+++ p4.py
@@ -12,3 +12,23 @@
 # made from the product of two 2-digit numbers is 9009 = 91 x 99.
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
+
+def isPalindrome(n):
+    if str(n) == str(n)[::-1]:
+        return True
+    else:
+        return False
+
+palindromes = []
+for i in range(999, 99, -1):
+    for j in range(999, 99, -1):
+        k = i * j
+        if isPalindrome(k):
+            if palindromes and k < max(palindromes):
+                break
+            else:
+                palindromes.append(k)
+                print(k)
+            
+palindromes.sort()
+print('Largest palindrome:', palindromes[-1])
